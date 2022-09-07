a2f22a15291571c950a0b6f5e74a825a6201662a
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-07 19:16:16 +0100

    changed name in dotenv file - wouldnt connect to db due to error

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
 const path = require('path');
+const routes = require('./controllers');
 const express = require('express');
-
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-
 const session = require('express-session');
+const hbs = exphbs.create({helpers});
+
+const app = express();
+const PORT = process.env.PORT || 3306;
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SECRET,
-    cookie: {},
-    resave: false,
+    secret: 'Super secret secret',
+    cookie: {
+        expires: 600 * 1000
+    },
+    resave: true,
+    rolling: true,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize,
-        checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
-        expiration: 1000 * 60 * 30 // will expire after 30 minutes
+        db: sequelize
     })
 };
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
 app.use(session(sess));
-const hbs = exphbs.create({
-    helpers
-});
+
+
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(require('./controllers/'));
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-    sequelize.sync({force: false  });
+app.use(routes);
+ sequelize.sync({force: false  }).then (() => {
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+   
 });
