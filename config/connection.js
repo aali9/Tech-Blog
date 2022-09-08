74a60d10344c87b0e42032a9b1bbede9e7772e37
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-09 00:09:58 +0100

    fixed bug in connection for db

--- config/connection.js
+++ config/connection.js
@@ -11,11 +11,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      host: DB_HOST,
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       port: 3306,
-      password: 'Bushraamin123?',
     }
   );
 }
