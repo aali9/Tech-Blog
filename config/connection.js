b994a0adc2794204d4739d19364bbc46447efff1
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-09 18:42:18 +0100

    couldnt connect db, trying to fix bug with jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,11 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: DB_HOST,
-      host: process.env.DB_HOST,
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
-    }
-  );
+      port: 3306
+  });
 }
 
 module.exports = sequelize;
